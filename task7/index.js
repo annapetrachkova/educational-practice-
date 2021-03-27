65881490ffbd92e274ba30c3a9d85471873b13b6
Анна Петрачкова <p.anytk@mail.ru>
2021-03-27 18:07:43 +0300

    docs: task7 done

---
+++ task7/index.js
@@ -0,0 +1,382 @@
+let adList = [
+    {
+        id: '1',
+        description: 'Ремонт стиральных машин',
+        createdAt: new Date('2021-02-01T11:00:00'),
+        link: 'http://stiralkino.by/',
+        vendor: 'Анна Петрачкова',
+        photoLink: 'https://raw.githubusercontent.com/annapetrachkova/educational-practice-/UI/UI/assets/remont.jpg',
+        validUntil: new Date('2021-05-01T24:00:00'),
+        discount: '15%',
+        hashTags: ['Бытовые услуги', 'Ремонт', 'Строительные работы', 'Прочие услуги']
+    },
+    {
+        id: '2',
+        description: 'Репетиторы',
+        createdAt: new Date('2021-01-01T11:00:00'),
+        link: 'https://profi-bel.by/repetitor/',
+        vendor: 'Степан',
+        photoLink: 'https://raw.githubusercontent.com/annapetrachkova/educational-practice-/UI/UI/assets/profi.jpg',
+        validUntil: new Date('2021-03-21T24:00:00'),
+        discount: '10%',
+        hashTags: ['Образовательные услуги', 'Прочие услуги']
+    },
+    {
+        id: '3',
+        description: 'Маникюр и педикюр',
+        createdAt: new Date('2020-12-12T11:00:00'),
+        link: 'https://fokanail.by/',
+        vendor: 'Алеся Хаткевич',
+        photoLink: 'https://raw.githubusercontent.com/annapetrachkova/educational-practice-/UI/UI/assets/nails.jpg',
+        validUntil: new Date('2021-03-31T24:00:00'),
+        discount: '30%',
+        hashTags: ['Красота и здоровье']
+    },
+    {
+        id: '4',
+        description: 'Клининговая компания',
+        createdAt: new Date('2020-12-01T11:00:00'),
+        link: 'https://clean-up.by/',
+        vendor: 'Евгения',
+        photoLink: 'https://raw.githubusercontent.com/annapetrachkova/educational-practice-/UI/UI/assets/clean.jpg',
+        validUntil: new Date('2021-04-09T24:00:00'),
+        discount: '15%',
+        hashTags: ['Бытовые услуги', 'Ремонт', 'Прочие услуги']
+    },
+    {
+        id: '5',
+        description: 'Услуги фотошопа',
+        createdAt: new Date('2020-11-14T11:00:00'),
+        link: 'https://www.prettyphoto.ru/',
+        vendor: 'ООО Боннети',
+        photoLink: 'https://raw.githubusercontent.com/annapetrachkova/educational-practice-/UI/UI/assets/ps.jpg',
+        validUntil: new Date('2021-09-14T24:00:00'),
+        discount: '50%',
+        hashTags: ['Фото и видеосъемка', 'Прочие услуги']
+    },
+    {
+        id: '6',
+        description: 'Помощь адвоката',
+        createdAt: new Date('2020-11-02T11:00:00'),
+        link: 'https://advokat-lawyer.by/',
+        vendor: 'ООО Боннети',
+        photoLink: 'https://raw.githubusercontent.com/annapetrachkova/educational-practice-/UI/UI/assets/lawyer.jpg',
+        validUntil: new Date('2021-07-02T24:00:00'),
+        discount: '20%',
+        hashTags: ['Юридические услуги']
+    },
+    {
+        id: '7',
+        description: 'Натяжные потолки',
+        createdAt: new Date('2020-10-09T11:00:00'),
+        link: 'https://houze.by/',
+        vendor: 'Алеся Хаткевич',
+        photoLink: 'https://raw.githubusercontent.com/annapetrachkova/educational-practice-/UI/UI/assets/ceiling.jpg',
+        validUntil: new Date('2021-05-09T24:00:00'),
+        discount: '30%',
+        hashTags: ['Ремонт', 'Строительные услуги', 'Бытовые услуги']
+    },
+    {
+        id: '8',
+        description: 'Спортивное питание',
+        createdAt: new Date('2020-09-01T11:00:00'),
+        link: 'https://gamma.by/',
+        vendor: 'Анна Петрачкова',
+        photoLink: 'https://raw.githubusercontent.com/annapetrachkova/educational-practice-/UI/UI/assets/sport.jpg',
+        validUntil: new Date('2021-09-01T24:00:00'),
+        discount: '5%',
+        hashTags: ['Прочие услуги']
+    },
+    {
+        id: '9',
+        description: 'Оклейка обоями',
+        createdAt: new Date('2020-09-01T11:00:00'),
+        link: 'https://domodel.by/categories/okleyka-oboyami',
+        vendor: 'Степан',
+        photoLink: 'https://raw.githubusercontent.com/annapetrachkova/educational-practice-/UI/UI/assets/wallpaper.jpg',
+        validUntil: new Date('2021-08-01T24:00:00'),
+        discount: '25%',
+        hashTags: ['Ремонт', 'Бытовые услуги', 'Строительные работы']
+    },
+    {
+        id: '10',
+        description: 'Макияж',
+        createdAt: new Date('2021-08-01T11:00:00'),
+        link: 'https://galinamakeup.by',
+        vendor: 'Евгения',
+        photoLink: 'https://raw.githubusercontent.com/annapetrachkova/educational-practice-/UI/UI/assets/makeup.jpg',
+        validUntil: new Date('2021-03-01T24:00:00'),
+        discount: '40%',
+        hashTags: ['Красота и здоровье']
+    },
+    {
+        id: '11',
+        description: 'Профессиональный шиномонтаж',
+        createdAt: new Date('2021-03-21T11:00:00'),
+        link: 'https://shinaminsk.by/',
+        vendor: 'Алеся Хаткевич',
+        photoLink: 'https://www.slivki.by/znijki-media/w522_322/default/1009921/1-shinomontazh-minsk-skidka-shinaminskby.jpg',
+        validUntil: new Date('2021-06-21T24:00:00'),
+        discount: '35%',
+        hashTags: ['Прочие услуги']
+    },
+    {
+        id: '12',
+        description: 'Акции в магазинах "Мила"',
+        createdAt: new Date('2021-02-20T11:00:00'),
+        link: 'https://mila.by/',
+        vendor: 'Мила',
+        photoLink: 'https://lh3.googleusercontent.com/proxy/KoRtNY-cYnGaLhjnKV-hLqggqiA4OXKI2WdvsbPcahmC_c8cTVgwJiac6h7lKRGDPO3b00Bsg_chbmPZNVcIjEosnrp2Fa8c94YdWJdJfkodE-j4',
+        validUntil: new Date('2021-04-20T24:00:00'),
+        discount: '15%',
+        hashTags: ['Красота и здоровье', 'Прочие услуги']
+    },
+    {
+        id: '13',
+        description: 'УЗИ для женщин и мужчин',
+        createdAt: new Date('2021-01-01T11:00:00'),
+        link: 'https://idealmed.by/',
+        vendor: 'Степан',
+        photoLink: 'https://aquaminskclinic.by/upload/iblock/800/800fcd272c7d1a27ce63c6b2cbab6118.png',
+        validUntil: new Date('2021-06-01T24:00:00'),
+        discount: '10%',
+        hashTags: ['Красота и здоровье', 'Прочие услуги']
+    },
+    {
+        id: '14',
+        description: 'Суши от Rollbox',
+        createdAt: new Date('2021-03-01T11:00:00'),
+        link: 'https://rollbox.by/',
+        vendor: 'Rollbox',
+        photoLink: 'https://sushichefarts.by/upload/iblock/ba7/ba79b531e47dfa1bec62fce7d1817916.jpg',
+        validUntil: new Date('2021-07-01T24:00:00'),
+        discount: '40%',
+        hashTags: ['Прочие услуги']
+    },
+    {
+        id: '15',
+        description: 'Ремонт телефонов',
+        createdAt: new Date('2021-02-28T11:00:00'),
+        link: 'https://applejam.by/',
+        vendor: 'Анна Петрачкова',
+        photoLink: 'https://yams.kufar.by/api/v1/kufar-ads/images/64/6433361110.jpg?rule=gallery',
+        validUntil: new Date('2021-07-28T24:00:00'),
+        discount: '20%',
+        hashTags: ['Ремонт', 'Прочие услуги']
+    },
+    {
+        id: '16',
+        description: 'Ремонт телефонов',
+        createdAt: new Date('2021-02-28T11:00:00'),
+        link: 'https://applejam.by/',
+        vendor: 'Анна Петрачкова',
+        photoLink: 'https://yams.kufar.by/api/v1/kufar-ads/images/64/6433361110.jpg?rule=gallery',
+        validUntil: new Date('2021-07-28T24:00:00'),
+        discount: '20%',
+        hashTags: ['Ремонт', 'Прочие услуги']
+    },
+    {
+        id: '17',
+        description: 'Ультразвуковая чистка, пилинги лица',
+        createdAt: new Date('2021-01-30T11:00:00'),
+        link: 'https://skinlux.by/',
+        vendor: '',
+        photoLink: 'https://hydrafacial.ru/wp-content/uploads/2017/12/uz-chistka-lica1.jpg',
+        validUntil: new Date('2021-07-30T24:00:00'),
+        discount: '10%',
+        hashTags: ['Красота и здоровье', 'Прочие услуги']
+    },
+    {
+        id: '18',
+        description: 'Скидки до 70% в магазинах Kari и Kari KIDS!',
+        createdAt: new Date('2021-03-01T11:00:00'),
+        link: 'https://by.kari.com/by/',
+        vendor: 'Kari',
+        photoLink: 'https://trk-pilot.ru/wp-content/uploads/2018/04/Logo_Kari_newest.jpg',
+        validUntil: new Date('2021-07-01T24:00:00'),
+        discount: '70%',
+        hashTags: ['Прочие услуги']
+    },
+    {
+        id: '19',
+        description: 'Скидки в магазинах 21vek',
+        createdAt: new Date('2021-03-08T11:00:00'),
+        link: 'https://www.21vek.by/',
+        vendor: '21vek',
+        photoLink: 'http://i.mycdn.me/i?r=AzGBqNaF5OQp2lMpnhRx4DEF7OiGMi6UuMkBd-_FgVClksjukuIIGj_LwIulBvzyPuY',
+        validUntil: new Date('2021-08-08T24:00:00'),
+        discount: '40%',
+        hashTags: ['Прочие услуги']
+    },
+    {
+        id: '20',
+        description: 'Обучение иностранным языкам в "SOL Minsk"',
+        createdAt: new Date('2021-03-20T11:00:00'),
+        link: 'https://sol.by/',
+        vendor: 'SOL',
+        photoLink: 'https://enguide.by/s/public/upload/images/e6ce/8bd6/f526/3246/470a/4b7d/f2ed/7df0.jpeg',
+        validUntil: new Date('2021-08-21T24:00:00'),
+        discount: '20%',
+        hashTags: ['Образовательные услуги']
+    }
+]
+
+class adCollection {
+    _adList  = []
+
+    constructor(adList){
+        this._adList = adList
+    }
+
+    getPage (skip = 0, top = 10, filterConfig = undefined) {
+        let elem = adList;
+        if (filterConfig) {
+            for (let item in filterConfig) {
+                if (item === 'createdAt') {
+                    elem = elem.filter(item => item.createdAt <= filterConfig.createdAt);
+                }
+                else if (item === 'validUntil') {
+                    elem = elem.filter(item => item.validUntil >= filterConfig.validUntil);
+                }
+                else if (item === 'vendor') {
+                    elem = elem.filter(item => item.vendor === filterConfig.vendor);
+                }
+                else {
+                    return 'Error'
+                }
+            }
+        }
+        elem.sort(function (a, b) {
+            return a.createdAt - b.createdAt;
+        });
+        return elem.slice(skip, skip + top);
+    }
+
+
+    get(id) {
+        if (id.valueOf() > 0) {
+            return adList.filter(item => item.id == id)
+        }
+        return false;
+    }
+
+    static _validate(elem) {
+        return typeof elem.id == 'string' && elem.id !== ""
+            && typeof elem.description == 'string' && elem.description.length <= 200 && typeof elem.link == 'string' && elem.description !== ""
+            && elem.createdAt instanceof Date && elem.createdAt !== ""
+            && typeof elem.link == 'string' && elem.link !== ""
+            && typeof elem.vendor == 'string' && elem.vendor !== ""
+            && elem.validUntil instanceof Date
+            && Array.isArray(elem.hashTags) && elem.hashTags !== ""
+            && typeof elem.discount == 'string' && elem.discount !== ""
+            && elem.validUntil instanceof Date && elem.validUntil !== ""
+    }
+
+    add(newElem) {
+        if (adCollection._validate(newElem)) {
+            for (let item of adList) {
+                if (item.id == newElem.id) {
+                    return 'Error'
+                }
+            }
+            adList.push(newElem)
+            return adList
+        }
+        else return false
+    }
+
+
+    edit(id, propertyChange, changedValue) {
+        if (propertyChange === 'id' || propertyChange === 'vendor' || propertyChange === 'createdAt') {
+            return 'Error'
+        }
+        for (let item of adList) {
+            if (item.id === id) {
+                for (let key in item) {
+                    if (key === propertyChange) {
+                        item[key] = changedValue
+                    }
+                }
+            }
+        }
+        return adList
+    }
+
+    remove(id) {
+        if (!isNaN(id)) {
+            for (let i = 0; i < adList.length; i++) {
+                if (adList[i].id == id) {
+                    adList.splice(i, 1)
+                    return adList
+                }
+            }
+        } else return false
+    }
+
+    addAll(array){
+        let result = []
+        for(let item of this._adList) {
+            if (adCollection._validate(item)) {
+                array.push(item)
+            }
+            else {
+                result.push(item)
+            }
+        }
+        return result
+    }
+
+    clear(array) {
+        return array = []
+    }
+
+}
+let collection = new adCollection(adList)
+let collection2 = []
+
+console.log(collection.addAll(collection2))
+console.log(collection.getPage(0, 10))
+console.log(collection.getPage(10, 10))
+console.log(collection.getPage(0, 4, {vendor: 'Анна Петрачкова'}))
+console.log(collection.getPage(0, 5, {validUntil: new Date('2021-06-01T24:00:00')}))
+
+console.log(collection.get(12))
+console.log(collection.get(22)) // покажет пустой массив потому что 22 не сущ
+
+console.log(collection.add({id: '21',
+    description: 'Суши от Rollbox',
+    createdAt: new Date('2021-03-01T11:00:00'),
+    link: 'https://rollbox.by/',
+    vendor: 'Rollbox',
+    photoLink: 'https://sushichefarts.by/upload/iblock/ba7/ba79b531e47dfa1bec62fce7d1817916.jpg',
+    validUntil: new Date('2021-07-01T24:00:00'),
+    discount: '40%',
+    hashTags: ['Прочие услуги']})) //объявление добавится
+console.log(collection.add({id: '14',
+    description: 'Суши от Rollbox',
+    createdAt: new Date('2021-03-01T11:00:00'),
+    link: 'https://rollbox.by/',
+    vendor: 'Rollbox',
+    photoLink: 'https://sushichefarts.by/upload/iblock/ba7/ba79b531e47dfa1bec62fce7d1817916.jpg',
+    validUntil: new Date('2021-07-01T24:00:00'),
+    discount: '40%',
+    hashTags: ['Прочие услуги']}))  // Error, 14 id уже есть
+
+console.log(collection.edit(4, 'discount', '70%'))
+console.log(collection.edit(16, 'id', '23')) // Error, нет такого id
+
+console.log(collection.remove(3)) // удалит объявление id = 3
+
+console.log(collection.clear(collection))
+
+
+
+
+
+
+
+
+
+
+
+
